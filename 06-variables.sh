2b5e9784c7cc82f36fb56ee35c68c56401c381ff
Rajesh <katamnenirajesh2.gmail.com>
2026-02-22 14:59:34 +0530

    actions

--- 06-variables.sh
+++ 06-variables.sh
@@ -7,5 +7,5 @@ echo "script eecuted at: $START_TIME"
 sleep 10
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(($START_TIME-$END_TIME))
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "script eecuted in: $TOTAL_TIME"

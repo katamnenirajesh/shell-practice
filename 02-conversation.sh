892052b424dd75431cee277993be7d6adf14fbc2
Rajesh <katamnenirajesh2.gmail.com>
2026-02-22 12:37:34 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Suresh:: Hi Ramesh how are you?"
+echo "Ramesh:: Hi Suresh I am fine,thanks for asking.How are you doing?"
+echo "Suresh:: doing fine.what are you learming?"
+echo "Ramesh:: Iam learning shell.What about you?"
+echo "Suresh:: Nothing"
